a5f6da75612629a6a994a78614a6bbef890a70f1
Lovisa Montelius <lovisamon@gmail.com>
2021-12-01 15:32:54 +0100

    Add project files.

---
+++ Strategy/MessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Design_Patterns_Assignment
+{
+    internal class MessageHandler
+    {
+        internal static void SendEmail(string message)
+        {
+            Console.WriteLine($"sending \"{message}\" as an email");
+        }
+
+        internal static void SendSMS(string message)
+        {
+            Console.WriteLine($"sending \"{message}\" as a sms");
+        }
+
+        internal static void SendFacebookMessage(string message)
+        {
+            Console.WriteLine($"sending \"{message}\" as a facebookmessage");
+        }
+    }
+}
---
+++ Strategy/Strategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Design_Patterns_Assignment
+{
+    internal class Strategy
+    {
+        internal static void Run()
+        {
+            // Refactor this code so that it uses the strategy Pattern
+            Console.WriteLine("Strategy");
+            var message = "This is the message";
+            MessageHandler.SendEmail(message);
+            MessageHandler.SendSMS(message);
+            MessageHandler.SendFacebookMessage(message);
+
+            // What i want to happen to happen is
+            //
+            // Change type of message to send to sms etc.
+            // send message            
+        }
+    }
+}
